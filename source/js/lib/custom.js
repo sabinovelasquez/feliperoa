087882500bcdbec4c3b0acc1e38ee9c7825c953d
Sabino Velasquez <sabino@Sabinos-Mac-mini.local>
2016-01-17 17:12:08 -0300

    first approach

--- source/js/lib/custom.js
+++ source/js/lib/custom.js
@@ -1,4 +1,21 @@
 'use strict';
+
 var app = angular
 
-	.module('app', ['ngAnimate','ui.bootstrap']);
+	.module('app', ['ngAnimate','ui.bootstrap', 'angular-parallax', 'duScroll'])
+
+	.controller('gralCtrl', [ '$scope', '$document', 
+		function($scope, $document) {
+			$scope.head = false;
+			$scope.lang = 1;
+			$document.on('scroll', function() {
+				if( $document.scrollTop() > 330 ){
+					$scope.head = true;
+				}else{
+					$scope.head = false;
+				}
+				$scope.$apply();
+			});
+		}
+		
+	]);
